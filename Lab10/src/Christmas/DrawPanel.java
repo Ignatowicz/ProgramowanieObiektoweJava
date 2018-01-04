c2c94b734c3df47225756e16f6c9820a63a4b5f5
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2018-01-04 14:19:02 +0100

    Lab11 getting started, Lab10 in progress

--- Lab10/src/Christmas/DrawPanel.java
+++ Lab10/src/Christmas/DrawPanel.java
@@ -44,35 +44,35 @@ public class DrawPanel extends JPanel {
     }
 
     void bubbles(){
-//        Bubble b1 = new Bubble(500,100,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b2 = new Bubble(450,200,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b3 = new Bubble(550,200,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b4 = new Bubble(400,300,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b5 = new Bubble(600,300,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b6 = new Bubble(500,300,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b7 = new Bubble(350,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b8 = new Bubble(650,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b9 = new Bubble(450,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        Bubble b10 = new Bubble(550,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
-//        shapes.add(b1);
-//        shapes.add(b2);
-//        shapes.add(b3);
-//        shapes.add(b4);
-//        shapes.add(b5);
-//        shapes.add(b6);
-//        shapes.add(b7);
-//        shapes.add(b8);
-//        shapes.add(b9);
-//        shapes.add(b10);
+        Bubble b1 = new Bubble(500,100,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b2 = new Bubble(450,200,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b3 = new Bubble(550,200,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b4 = new Bubble(400,300,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b5 = new Bubble(600,300,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b6 = new Bubble(500,300,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b7 = new Bubble(350,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b8 = new Bubble(650,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b9 = new Bubble(450,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        Bubble b10 = new Bubble(550,400,0.5, new Color(10,100,100,100), new Color(0,0,0));
+        shapes.add(b1);
+        shapes.add(b2);
+        shapes.add(b3);
+        shapes.add(b4);
+        shapes.add(b5);
+        shapes.add(b6);
+        shapes.add(b7);
+        shapes.add(b8);
+        shapes.add(b9);
+        shapes.add(b10);
     }
 
     void branches(){
-        //Branch b1 = new Branch(254,30,320, 130, 1, new Color(7, 126, 11));
-        //Branch b2 = new Branch(574,160,0, 0, 1, new Color(7, 126, 11));
+        Branch b1 = new Branch(254,30,320, 130, 1, new Color(7, 126, 11));
+        Branch b2 = new Branch(574,160,0, 0, 1, new Color(7, 126, 11));
 
 
-        //shapes.add(b1);
-        //shapes.add(b2);
+        shapes.add(b1);
+        shapes.add(b2);
 
 
     }
