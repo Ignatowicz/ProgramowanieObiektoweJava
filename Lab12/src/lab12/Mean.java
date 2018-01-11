e4bf3bbc84445948881352c1b04e8cffb1c102df
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2018-01-11 14:12:10 +0100

    Lab12 - getting started

---
+++ Lab12/src/lab12/Mean.java
@@ -0,0 +1,16 @@
+package lab12;
+
+public class Mean {
+    static double[] array;
+    static void initArray(int size){
+        array = new double[size];
+        for(int i=0;i<size;i++){
+            array[i]= Math.random()*size/(i+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        initArray(100000000);
+    }
+
+}
